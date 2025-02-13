28a3207e57122d2fec4274bb4024ba5ce75e01d7
Amaan Ul Haq Siddiqui <amaanulhaq.s@outlook.com>
2025-02-13 22:59:01 +0500

    feat: add EC2 instance module configuration with AWS provider and security settings

---
+++ final/ec2full.tf
@@ -0,0 +1,29 @@
+module "ec2_instance" {
+  source = "git::https://github.com/amaan-igs/terraform-aws-ec2-instance.git"
+
+  ami_id                = "ami-00bb6a80f01f03502"
+  instance_type         = "t2.micro"
+  subnet_id             = "subnet-0194c32a03d44fdbd"
+  security_group_ids    = ["sg-07ea31bd8b8dab9f1"]
+  instance_name         = "prod-wayz"
+  key_name              = "sudo"
+  iam_instance_profile  = "instance-profile-igs" 
+  root_volume_size      = 9  
+  root_volume_type      = "gp2"
+  root_volume_encryption = true
+  enable_imdsv2         = true
+
+  ebs_volumes = [
+    {
+      device_name = "/dev/xvdf"
+      volume_size = 20
+      volume_type = "gp2"
+      encrypted   = true
+    }
+  ]
+
+  tags = {
+    Environment = "prod"
+    Project     = "wayz-project"
+  }
+}
